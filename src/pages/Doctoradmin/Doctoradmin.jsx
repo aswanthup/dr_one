336e09e106c9f2b436b341a59b79992d408bfa18
aswanthup <aswanth@chaavie.com>
2024-03-05 10:44:50 +0530

    doctor admin

--- src/pages/Doctoradmin/Doctoradmin.jsx
+++ src/pages/Doctoradmin/Doctoradmin.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import Hospitaladminnotification from '../../components/Hospitaladminnotification'
 import Rightnavbar from '../../components/Rightnavbar'
+import "../Doctoradmin/doctoradmin.css";
+
+
+
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+
+const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//  backgroundColor:"white",
+// padding:'2px',
+ 
+
+};
+
+
 
 export default function Doctoradmin() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
-    <div>
+<div>
 
 
 
@@ -75,11 +102,75 @@ export default function Doctoradmin() {
 </div> */}
 
 
-</div>
+<div className="availability">
+<h3>Availability</h3>
+ 
+  <div className="available flex">
+      <div className="hospitalname">
+         <h4>
+          home
+         </h4>
+      </div>
+
+
+      <div onClick={handleOpen} className="hospitalname_button">
+         <h4>
+          View
+         </h4>
+      </div>
+
+
+  </div>
+
+
+  <div className="available flex ">
+      <div className="hospitalname">
+         <h4>
+          Aster Mims Hospital
+         </h4>
+      </div>
+
+      <div className="hospitalname_button">
+         <h4>
+          View
+         </h4>
+      </div>
+
+
+  </div>
+
+  <div className="available_add_button flex">
+    <h4> Add More </h4>
+  </div>
 
 
 </div>
 
+
+
+<div>
+   
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+   <div style={style} className='viewdetails'>
+        <h3>Availability</h3>
+        <h4>Hospital Name</h4>
+        <input type="text" />
+
+   </div>
+      </Modal>
     </div>
+
+
+</div>
+
+
+</div>
+
+</div>
   )
 }
