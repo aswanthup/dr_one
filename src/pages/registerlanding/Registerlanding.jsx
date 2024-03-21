edac4c13858005fbf0a559267a51225717404217
aswanthup <aswanth@chaavie.com>
2024-03-21 15:30:59 +0530

    new

--- src/pages/registerlanding/Registerlanding.jsx
+++ src/pages/registerlanding/Registerlanding.jsx
@@ -5,27 +5,27 @@ export default function Registerlanding() {
     const navigate=useNavigate()
   return (
     <div className="registerlanding">
-      <div className="container maintitle flex">
+      <div className="container10 maintitle flex">
         <h1>Register As</h1>
       </div>
 
-      <div className="container registerlanding_main flex">
-        <div className="registerlanding_left">
+      <div className="container10 registerlanding_main flex">
+        <div  onClick={()=>navigate("/doctoradminregistration1")}  className="registerlanding_left">
           <div className="registerlanding_image flex">
             <img src="images/mobile.png" alt="" />
           </div>
 
-          <div onClick={()=>navigate("/doctoradminregistration1")} className="registerlanding_button flex">
+          <div className="registerlanding_button flex">
             <h2>Doctor</h2>
           </div>
         </div>
 
-        <div className="registerlanding_left">
+        <div  onClick={()=>navigate("/hospitaladminregistration1")}  className="registerlanding_left">
           <div className="registerlanding_image flex">
             <img src="/images/hospital10.jpg" alt="" />
           </div>
 
-          <div onClick={()=>navigate("/hospitaladminregistration1")}  className="registerlanding_button flex">
+          <div className="registerlanding_button flex">
             <h2>Hospital</h2>
           </div>
         </div>
