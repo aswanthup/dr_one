a56ad0d549d1a28fb8f10feb59cefca8f86d6e56
aswin <aswinkasaas@gmail.com>
2024-05-27 13:04:08 +0530

    Add availabilityin doctor profile

--- src/pages/User/Userprofile.jsx
+++ src/pages/User/Userprofile.jsx
@@ -8,71 +8,71 @@ import Footer from '../../components/Footer';
 export default function Userprofile() {
 
 
-    
 
-  return (
-    <div>
 
-<Headroom>
-<Navbar /> 
-</Headroom>
-       <div className="container-third">
+    return (
+        <div>
 
-        <div className="editing-section flex">
-<div>
-            <div className="photo-section flex">
-                <img src="images/man.jpg" alt="" />
-<div className="img-button-section flex">
-    <a href="" className='flex'><h4>Upload Photo</h4></a>
-</div>
+            <Headroom>
+                <Navbar />
+            </Headroom>
+            <div className="container-third">
+
+                <div className="editing-section flex">
+                    <div>
+                        <div className="photo-section flex">
+                            <img src="images/man.jpg" alt="" />
+                            <div className="img-button-section flex">
+                                <a href="" className='flex'><h4>Upload Photo</h4></a>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                    <div className="editing-data-section">
+                        <h1>Edit Your <span className='color-blue'>Profile</span></h1>
+                        <div className="editing-input-section">
+                            <div className="editing-input-label">
+                                <h4>Name</h4>
+                                <input type="text" placeholder='Arun' />
+                            </div>
+
+                            <div className="editing-input-label">
+                                <h4>Second Name</h4>
+                                <input type="text" placeholder='Kumar' />
+                            </div>
+
+
+                            <div className="editing-input-label">
+                                <h4>Phone Number</h4>
+                                <input type="text" placeholder='+91 8958367392' />
+                            </div>
+
+                            <div className="editing-input-label">
+                                <h4>Email</h4>
+                                <input type="text" placeholder='arunkumar@gmail.com' />
+                            </div>
+
+                            <div className="editing-input-label">
+                                <h4>Password</h4>
+                                <input type="text" placeholder='arun@123' />
+                            </div>
+
+                            <div className="editing-button-section flex">
+                                <a href=""><h4 className='cancel-edit'>Cancel</h4></a>
+                                <a href=""><h4 className='cancel-edit2'>Save</h4></a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
 
             </div>
-            </div>
 
-
-
-            <div className="editing-data-section">
-                <h1>Edit Your <span className='color-blue'>Profile</span></h1>
-<div className="editing-input-section">
-    <div className="editing-input-label">
-        <h4>Name</h4>
-        <input type="text" placeholder='Arun' />
-    </div>
-
-    <div className="editing-input-label">
-        <h4>Second Name</h4>
-        <input type="text" placeholder='Kumar' />
-    </div>
-
-
-    <div className="editing-input-label">
-        <h4>Phone Number</h4>
-        <input type="text" placeholder='+91 8958367392' />
-    </div>
-
-    <div className="editing-input-label">
-        <h4>Email</h4>
-        <input type="text" placeholder='arunkumar@gmail.com' />
-    </div>
-
-    <div className="editing-input-label">
-        <h4>Password</h4>
-        <input type="text" placeholder='arun@123' />
-    </div>
-
-    <div className="editing-button-section flex">
-                <a href=""><h4 className='cancel-edit'>Cancel</h4></a>
-                <a href=""><h4  className='cancel-edit2'>Save</h4></a>
-    </div>
-</div>
-
-            </div>
-
+            <Footer />
         </div>
-
-       </div>
-
-<Footer />
-    </div>
-  )
+    )
 }
--- src/pages/doctor/Doctor_profile.jsx
+++ src/pages/doctor/Doctor_profile.jsx
@@ -5,52 +5,53 @@ import Navbar from '../../components/Navbar'
 import { useLocation } from 'react-router-dom';
 
 export default function Doctor_profile() {
+  const days = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  ]
   const location = useLocation();
-  const doctor = location?.state
+  // const doctor = location?.state
+  const doctor = ''
   console.log(doctor)
   return (
     <div>
       <Navbar />
-      <div className="container">
-        <div className="doctor-profile-section">
+      <div className='Doctor_profileAlign'>
 
-          <div className="doctor-profile-photo">
-            <img src={doctor?.image || `images/doc.jpg`} alt="" />
-          </div>
+        <div className="container Doctor_profileAlignCenter">
+          <div className="doctor-profile-section">
+
+            <div className="doctor-profile-photo">
+              <img src={doctor?.image || `images/doc.jpg`} alt="" />
+            </div>
 
 
-          <div className="doctor-profile-left-right flex">
-            <div className="doctor-profile-left flex">
-              <h2 style={{ color: 'white' }}>{doctor?.name}</h2>
-              <div className="doctorprofilestar flex">
+            <div className="doctor-profile-left-right flex">
+              <div className="doctor-profile-left flex">
+                <h2 style={{ color: 'white' }}>{doctor?.name}</h2>
+                <div className="doctorprofilestar flex">
+                  <i className="ri-star-fill" />
+                  <i className="ri-star-fill" />
+                  <i className="ri-star-fill" />
+                  <i className="ri-star-fill" />
+                  <i className="ri-star-fill" />
+                </div>
 
-                <i className="ri-star-fill" />
-                <i className="ri-star-fill" />
-                <i className="ri-star-fill" />
-                <i className="ri-star-fill" />
-                <i className="ri-star-fill" />
+                <h4> {doctor?.education_qualification || `BDS.MDS-Prosthodontist`} </h4>
+                <h4>  {new Date().getFullYear() - (doctor?.experience || new Date().getFullYear())}
+                  <span style={{ paddingLeft: 4 }}>
+                    Year Experience
+                  </span></h4>
+                <h4> {doctor?.specialization || `Dentist,Cosmetic`}</h4>
+              </div>
+              <div className="doctor-profile-right">
+                <h4>
+                  {doctor?.about || "Dr. Rohith Rajashekhar is a Dentist,Restorative Dentist and Cosmetic/Aesthetic Dentist in T Dasarahalli, Bangalore and has an experience of 6 years in these fields. Dr. Rohith Rajashekhar practices at Partha Dental Skin Hair in T Dasarahalli, Bangalore. He completed BDS from Rajiv Gandhi University of Health Sciences in 2017 and MDS - Prosthodontist And Crown Bridge from Rajiv Gandhi University of Health Sciences in 2022"}
+                </h4>
+
 
 
               </div>
 
-              <h4> {doctor?.education_qualification || `BDS.MDS-Prosthodontist`} </h4>
-              <h4>  {new Date().getFullYear() - (doctor?.experience || new Date().getFullYear())}
-                <span style={{ paddingLeft: 4 }}>
-                  Year Experience
-                </span></h4>
-              <h4> {doctor?.specialization || `Dentist,Cosmetic`}</h4>
-
-
-
-            </div>
-
-
-
-            <div className="doctor-profile-right">
-              <h4>
-                {doctor?.about || "Dr. Rohith Rajashekhar is a Dentist,Restorative Dentist and Cosmetic/Aesthetic Dentist in T Dasarahalli, Bangalore and has an experience of 6 years in these fields. Dr. Rohith Rajashekhar practices at Partha Dental Skin Hair in T Dasarahalli, Bangalore. He completed BDS from Rajiv Gandhi University of Health Sciences in 2017 and MDS - Prosthodontist And Crown Bridge from Rajiv Gandhi University of Health Sciences in 2022"}
-              </h4>
-
 
 
             </div>
@@ -58,31 +59,50 @@ export default function Doctor_profile() {
 
 
           </div>
-
-          <div className="doctor-profile-contact flex">
-
-
-
-            <div className="doctor-time flex">
-              <h4>Available : 9.00 - 4.00pm</h4>
+          <div className='DoctorAvailableSec'>
+            <div className='DoctorAvailableAvailableTag'>
+              <p>Available</p>
             </div>
-
-            <div className="doctor-contact-button flex">
-
-              <a href=""><h4 className='flex'><div><i class="ri-phone-line"></i></div>Contact</h4></a>
+            <div className='DoctorAvailableAvailableSec'>
+              <div className='DoctorAvailableSecSettingSec'>
+                <div className='DoctorAvailableTiming'>
+                  <p>Caritas hospital, Kotayam</p>
+                  <div className='DoctorAvailableTimingDays'>
+                    {days.map((day, index) =>
+                      <p>{day.slice(0, 3)}<span>{index === days?.length - 1 ? '' : ","}</span></p>
+                    )}
+                  </div>
+                </div>
+                <button>View Details</button>
+              </div>
+              <div className='DoctorAvailableSecSettingSec'>
+                <div className='DoctorAvailableTiming'>
+                  <p>Caritas hospital, Kotayam</p>
+                  <div className='DoctorAvailableTimingDays'>
+                    {days.map((day, index) =>
+                      <p>{day.slice(0, 3)}<span>{index === days?.length - 1 ? '' : ","}</span></p>
+                    )}
+                  </div>
+                </div>
+                <button>View Details</button>
+              </div>
+              <div className='DoctorAvailableSecSettingSec'>
+                <div className='DoctorAvailableTiming'>
+                  <p>Caritas hospital, Kotayam</p>
+                  <div className='DoctorAvailableTimingDays'>
+                    {days.map((day, index) =>
+                      <p>{day.slice(0, 3)}<span>{index === days?.length - 1 ? '' : ","}</span></p>
+                    )}
+                  </div>
+                </div>
+                <button>View Details</button>
+              </div>
             </div>
-
-
-
-
-
-
 
           </div>
-
         </div>
-      </div>
 
+      </div>
 
       <Footer />
 
