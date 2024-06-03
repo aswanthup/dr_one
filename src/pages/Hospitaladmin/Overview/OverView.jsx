6b7ba8fede76c96967d3a1d37fd3e63680fbed60
ChaavieNandakishor <chaavienandakishor@gmail.com>
2024-06-03 12:53:00 +0530

    added Hospital dashboard

---
+++ src/pages/Hospitaladmin/Overview/OverView.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+
+function Overview() {
+  return (
+    <>
+      <div className="mainadmindoctordatas flex">
+        <div className="mainadmindoctordatas_profile flex">
+          <img
+            className="mainadmindoctordatas_profile_photo"
+            src="/images/doc.jpg"
+            alt=""
+          />
+
+          <div className="mainadmindoctordatas_profile_data flex">
+            <div className="flex">
+              {" "}
+              <h2>Prime Medical Hospital</h2>{" "}
+              <h4
+                className="highlight_data"
+                style={{
+                  background: "#2A9D8F",
+                  color: "white",
+                  marginLeft: "10px",
+                }}
+              >
+                Homeopathy
+              </h4>
+            </div>
+
+            <h4
+              className="highlight_data"
+              style={{ background: "#3A65FD", color: "white" }}
+            >
+              LIP874657467
+            </h4>
+
+            <div className="flex">
+              <div className="flex texticonset">
+                <i class="fi fi-sr-call-outgoing"></i>
+                <h4 style={{ marginLeft: "10px" }}>+91 9878898346</h4>
+              </div>
+            </div>
+
+            <div className="flex texticonset">
+              <i class="fi fi-sr-envelope"></i>
+              <h4 style={{ marginLeft: "10px" }}>anilyadhav@gmail.com</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="mainadmindoctordatas_chart flex">
+          <div className="mainadmindoctordatas_chart1 flex">
+            <div className="mainadmindoctordatas_chart_icon flex">
+              <i class="fi fi-sr-call-outgoing"></i>
+            </div>
+
+            <div style={{ marginLeft: "18px" }}>
+              <h2>200</h2>
+              <h4>Views</h4>
+            </div>
+          </div>
+
+          <div className="mainadmindoctordatas_chart1 mainadmindoctordatas_chart2 flex">
+            <div className="mainadmindoctordatas_chart_icon flex">
+              <i class="fi fi-sr-call-outgoing"></i>
+            </div>
+
+            <div style={{ marginLeft: "18px" }}>
+              <h2>200</h2>
+              <h4>Views</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="photosdivadmin">
+        <h3 style={{ marginBottom: "1.3vw" }}>About</h3>
+        <div className="photosdivadminsection flex">
+          <img src="/images/doc.jpg" alt="" />
+          <img src="/images/doc.jpg" alt="" />
+          <img src="/images/doc.jpg" alt="" />
+        </div>
+      </div>
+
+      <div className="mainadmindoctoraboutavail flex">
+        <div className="mainadmindoctorabout ">
+          <h3 style={{ marginBottom: "1.3vw" }}>About</h3>
+
+          <h4 style={{ marginBottom: "1.3vw" }}>
+            Open-source neutral-style system symbols elaborately crafted for
+            designers and developers. All of the icons are free for both
+            personal and commercial use.
+          </h4>
+          <h3 style={{ marginBottom: "1.3vw" }}>Address</h3>
+
+          <h4 style={{ marginBottom: "1vw" }}>
+            Fifth Floor Arcadia Market Sec 49 South City 2, Gurgaon
+          </h4>
+          <div className="flex adimindoctorpin">
+            <h4 style={{ background: "#3A65FD", color: "white" }}>986744</h4>
+            <h4 style={{ background: "#F3F6FF", color: "#6B8CFE" }}>
+              Kozhikode
+            </h4>
+          </div>
+        </div>
+
+        <div className="mainadmindoctoravilability mainadmindoctoravilability2">
+          <div className="admin_fea_avai flex">
+            <div className="admin_fea_avai_left">
+              <h3 style={{ marginBottom: "1.3vw" }}>Features</h3>
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+            </div>
+
+            <div className="admin_fea_avai_right">
+              <h3 style={{ marginBottom: "1.3vw" }}>Specialities</h3>
+
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+              <h4 style={{ marginBottom: "1.3vw" }}>
+                <i class="ri-arrow-right-circle-fill"></i>Features 1
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Overview;
