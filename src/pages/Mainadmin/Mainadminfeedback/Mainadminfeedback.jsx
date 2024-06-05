5733266de8ecff0741d7f9e1ebb3d8dc99784338
aswanthup <aswanth@chaavie.com>
2024-06-05 18:14:31 +0530

    aswanth

--- src/pages/Mainadmin/Mainadminfeedback/Mainadminfeedback.jsx
+++ src/pages/Mainadmin/Mainadminfeedback/Mainadminfeedback.jsx
@@ -2,6 +2,516 @@ import React from 'react'
 
 export default function Mainadminfeedback() {
   return (
-    <div>Mainadminfeedback</div>
+    <div>
+        <div style={{marginTop:"1.3vw"}} className='flex admin_view_more'>
+    <h3>Views</h3>
+    <h4>Filter<i style={{marginLeft:"0.5vw"}} class="ri-equalizer-3-line"></i></h4>
+    </div>
+
+
+      <div className="feedbacksection flex">
+
+        <div className="feedbacksectioncard">
+            
+        <div className="feedbacksectioncardtop flex">
+            
+            <img src="/images/doc.jpg" alt="" />
+            <div style={{marginLeft:"10px"}}>
+              <h3>Aswanth Up</h3>
+              <h4>3/4/2024</h4>
+            </div>
+
+
+            </div>
+
+            <div className="feedbacksectioncardrewiew">
+
+              <div className='feedbacksectioncardrewiewstar flex'>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              </div>
+              <div className='feedbacksectioncardrewiewpara'> 
+               
+               <div  className='feedbacksectioncardrewiewpara2'  >           
+              <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+              </div> 
+              </div>
+  
+            </div>
+
+            
+            <div className="feedbacksectioncardbutton flex">
+
+              <div className="photoandtitle flex">
+                <img src="/images/doc.jpg" alt="" />
+                <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+              </div>
+
+              <div className="photoandtitle flex">
+                <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+                <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+              </div>
+            
+            
+
+
+            </div>
+
+
+
+        </div>
+
+        
+        <div className="feedbacksectioncard">
+            
+            <div className="feedbacksectioncardtop flex">
+                
+                <img src="/images/doc.jpg" alt="" />
+                <div style={{marginLeft:"10px"}}>
+                  <h3>Aswanth Up</h3>
+                  <h4>3/4/2024</h4>
+                </div>
+    
+    
+                </div>
+    
+                <div className="feedbacksectioncardrewiew">
+    
+                  <div className='feedbacksectioncardrewiewstar flex'>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  </div>
+                  <div className='feedbacksectioncardrewiewpara'> 
+                   
+                   <div  className='feedbacksectioncardrewiewpara2'  >           
+                  <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+                  </div> 
+                  </div>
+      
+                </div>
+    
+                
+                <div className="feedbacksectioncardbutton flex">
+    
+                  <div className="photoandtitle flex">
+                    <img src="/images/doc.jpg" alt="" />
+                    <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+                  </div>
+    
+                  <div className="photoandtitle flex">
+                    <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+                    <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+                  </div>
+                
+                
+    
+    
+                </div>
+    
+    
+    
+            </div>
+    
+
+            <div className="feedbacksectioncard">
+            
+            <div className="feedbacksectioncardtop flex">
+                
+                <img src="/images/doc.jpg" alt="" />
+                <div style={{marginLeft:"10px"}}>
+                  <h3>Aswanth Up</h3>
+                  <h4>3/4/2024</h4>
+                </div>
+    
+    
+                </div>
+    
+                <div className="feedbacksectioncardrewiew">
+    
+                  <div className='feedbacksectioncardrewiewstar flex'>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  </div>
+                  <div className='feedbacksectioncardrewiewpara'> 
+                   
+                   <div  className='feedbacksectioncardrewiewpara2'  >           
+                  <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+                  </div> 
+                  </div>
+      
+                </div>
+    
+                
+                <div className="feedbacksectioncardbutton flex">
+    
+                  <div className="photoandtitle flex">
+                    <img src="/images/doc.jpg" alt="" />
+                    <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+                  </div>
+    
+                  <div className="photoandtitle flex">
+                    <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+                    <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+                  </div>
+                
+                
+    
+    
+                </div>
+    
+    
+    
+            </div>
+      </div>
+      <div className="feedbacksection flex">
+
+<div className="feedbacksectioncard">
+    
+<div className="feedbacksectioncardtop flex">
+    
+    <img src="/images/doc.jpg" alt="" />
+    <div style={{marginLeft:"10px"}}>
+      <h3>Aswanth Up</h3>
+      <h4>3/4/2024</h4>
+    </div>
+
+
+    </div>
+
+    <div className="feedbacksectioncardrewiew">
+
+      <div className='feedbacksectioncardrewiewstar flex'>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      </div>
+      <div className='feedbacksectioncardrewiewpara'> 
+       
+       <div  className='feedbacksectioncardrewiewpara2'  >           
+      <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+      </div> 
+      </div>
+
+    </div>
+
+    
+    <div className="feedbacksectioncardbutton flex">
+
+      <div className="photoandtitle flex">
+        <img src="/images/doc.jpg" alt="" />
+        <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+      </div>
+
+      <div className="photoandtitle flex">
+        <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+        <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+      </div>
+    
+    
+
+
+    </div>
+
+
+
+</div>
+
+
+<div className="feedbacksectioncard">
+    
+    <div className="feedbacksectioncardtop flex">
+        
+        <img src="/images/doc.jpg" alt="" />
+        <div style={{marginLeft:"10px"}}>
+          <h3>Aswanth Up</h3>
+          <h4>3/4/2024</h4>
+        </div>
+
+
+        </div>
+
+        <div className="feedbacksectioncardrewiew">
+
+          <div className='feedbacksectioncardrewiewstar flex'>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          </div>
+          <div className='feedbacksectioncardrewiewpara'> 
+           
+           <div  className='feedbacksectioncardrewiewpara2'  >           
+          <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+          </div> 
+          </div>
+
+        </div>
+
+        
+        <div className="feedbacksectioncardbutton flex">
+
+          <div className="photoandtitle flex">
+            <img src="/images/doc.jpg" alt="" />
+            <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+          </div>
+
+          <div className="photoandtitle flex">
+            <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+            <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+          </div>
+        
+        
+
+
+        </div>
+
+
+
+    </div>
+
+
+    <div className="feedbacksectioncard">
+    
+    <div className="feedbacksectioncardtop flex">
+        
+        <img src="/images/doc.jpg" alt="" />
+        <div style={{marginLeft:"10px"}}>
+          <h3>Aswanth Up</h3>
+          <h4>3/4/2024</h4>
+        </div>
+
+
+        </div>
+
+        <div className="feedbacksectioncardrewiew">
+
+          <div className='feedbacksectioncardrewiewstar flex'>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          </div>
+          <div className='feedbacksectioncardrewiewpara'> 
+           
+           <div  className='feedbacksectioncardrewiewpara2'  >           
+          <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+          </div> 
+          </div>
+
+        </div>
+
+        
+        <div className="feedbacksectioncardbutton flex">
+
+          <div className="photoandtitle flex">
+            <img src="/images/doc.jpg" alt="" />
+            <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+          </div>
+
+          <div className="photoandtitle flex">
+            <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+            <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+          </div>
+        
+        
+
+
+        </div>
+
+
+
+    </div>
+</div>
+
+<div className="feedbacksection flex">
+
+<div className="feedbacksectioncard">
+    
+<div className="feedbacksectioncardtop flex">
+    
+    <img src="/images/doc.jpg" alt="" />
+    <div style={{marginLeft:"10px"}}>
+      <h3>Aswanth Up</h3>
+      <h4>3/4/2024</h4>
+    </div>
+
+
+    </div>
+
+    <div className="feedbacksectioncardrewiew">
+
+      <div className='feedbacksectioncardrewiewstar flex'>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      <i class="ri-star-fill"></i>
+      </div>
+      <div className='feedbacksectioncardrewiewpara'> 
+       
+       <div  className='feedbacksectioncardrewiewpara2'  >           
+      <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+      </div> 
+      </div>
+
+    </div>
+
+    
+    <div className="feedbacksectioncardbutton flex">
+
+      <div className="photoandtitle flex">
+        <img src="/images/doc.jpg" alt="" />
+        <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+      </div>
+
+      <div className="photoandtitle flex">
+        <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+        <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+      </div>
+    
+    
+
+
+    </div>
+
+
+
+</div>
+
+
+<div className="feedbacksectioncard">
+    
+    <div className="feedbacksectioncardtop flex">
+        
+        <img src="/images/doc.jpg" alt="" />
+        <div style={{marginLeft:"10px"}}>
+          <h3>Aswanth Up</h3>
+          <h4>3/4/2024</h4>
+        </div>
+
+
+        </div>
+
+        <div className="feedbacksectioncardrewiew">
+
+          <div className='feedbacksectioncardrewiewstar flex'>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          </div>
+          <div className='feedbacksectioncardrewiewpara'> 
+           
+           <div  className='feedbacksectioncardrewiewpara2'  >           
+          <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+          </div> 
+          </div>
+
+        </div>
+
+        
+        <div className="feedbacksectioncardbutton flex">
+
+          <div className="photoandtitle flex">
+            <img src="/images/doc.jpg" alt="" />
+            <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+          </div>
+
+          <div className="photoandtitle flex">
+            <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+            <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+          </div>
+        
+        
+
+
+        </div>
+
+
+
+    </div>
+
+
+    <div className="feedbacksectioncard">
+    
+    <div className="feedbacksectioncardtop flex">
+        
+        <img src="/images/doc.jpg" alt="" />
+        <div style={{marginLeft:"10px"}}>
+          <h3>Aswanth Up</h3>
+          <h4>3/4/2024</h4>
+        </div>
+
+
+        </div>
+
+        <div className="feedbacksectioncardrewiew">
+
+          <div className='feedbacksectioncardrewiewstar flex'>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          <i class="ri-star-fill"></i>
+          </div>
+          <div className='feedbacksectioncardrewiewpara'> 
+           
+           <div  className='feedbacksectioncardrewiewpara2'  >           
+          <h4>A doctor is a highly trained medical professional dedicated to diagnosing, treating, and preventing illnesses and injuries. With years of rigorous education and clinical experience, doctors possess a deep understanding of the human body and its complex systems. They play a crucial role in maintaining public health, providing care and support to patients, and often working long hours in challenging environments. Beyond their tech</h4>
+          </div> 
+          </div>
+
+        </div>
+
+        
+        <div className="feedbacksectioncardbutton flex">
+
+          <div className="photoandtitle flex">
+            <img src="/images/doc.jpg" alt="" />
+            <h3 style={{marginLeft:"10px"}}>Dr.Rani Vasudhev</h3>
+          </div>
+
+          <div className="photoandtitle flex">
+            <h4 style={{backgroundColor:"#F35454"}}>Reject</h4>
+            <h4 style={{backgroundColor:"#2A9D8F"}}>Approve</h4>
+          </div>
+        
+        
+
+
+        </div>
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
   )
 }
